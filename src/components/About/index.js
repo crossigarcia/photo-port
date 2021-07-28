2d53fb7ddcf34dedff66ee0d260b473e78a4dcd9
crosgar <cecilia.rossi.garcia@gmail.com>
2021-07-28 16:34:21 -0700

    added hooks, added Gallery component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,9 @@ function About() {
       <section className="my-5">
          <h1 id="about">Who am I?</h1>
          <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+         <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquam dapibus in posuere cubilia finibus. Amagna phasellus primis mus finibus. Dignissim lectus 
+            parturient tempor blandit sollicitudin parturient. Volutpat morbi duis auctor magnis per praesent proin. Fusce torquent eros placerat netus arcu. 
+            Volutpat donec et consectetur sapien erat praesent nam.</p>
       </section>
    );
 }
